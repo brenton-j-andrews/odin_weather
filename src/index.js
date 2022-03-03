c87b3a54e57bd0d614a4404d61c0f3cd5394672e
brent <brenton.j.andrews@gmail.com>
2022-03-03 10:58:05 -0700

    Unit toggle feature complete, more testing required

--- src/index.js
+++ src/index.js
@@ -2,39 +2,42 @@ import fetchWeatherData from "./modules/weather";
 import * as domFunctions from "./modules/dom";
 
 // Default settings.
-let default_location = true;
 let isMetric = false;
 
 
 let search_button = document.getElementById("search_btn");
-search_button.addEventListener("click", loadWeather);
+search_button.addEventListener("click", function() {
+    let city = document.getElementById("location_input").value;
+    loadWeather(city, 0);
+});
+
 
 let toggle_units_button = document.getElementById("toggle_units_btn");
 toggle_units_button.addEventListener("click", function() {
+    let city = document.getElementById("location_header").innerText;
     domFunctions.toggleUnits(isMetric);
+
+    if (isMetric) {
+        loadWeather(city, 0);
+    } else { 
+        loadWeather(city, 1);
+    }
+
     isMetric = !isMetric;
 });
 
 
-async function loadWeather() {
+async function loadWeather(city, unit_index) {
 
     // Build API URL.
-    let location;
-    if (default_location) {
-        location = "Salt Lake City";
-        default_location = false;
-    } 
-    
-    else location = document.getElementById("location_input").value;
-
     let key = "40117561b027d65aef26e6f9f3621abe";
-    let api_url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=${key}`;
+    let units = ["imperial", "metric"];
+    let api_url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units[unit_index]}&APPID=${key}`;
 
     // Pass URL, receive weather data object!
     let weatherData = await fetchWeatherData(api_url);
-    console.log(weatherData);
     domFunctions.updateDOM(weatherData);
 }
 
-// Initial function call to populate page upon loading.
-loadWeather();
+// Initial function call to populate page upon loading. Default load with imperial units.
+loadWeather("Salt Lake City", 0);
--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,6 +1,6 @@
 // Module for adding weather data to the DOM!
 function updateDOM(weatherData) {
-    let location_header = document.getElementById("location");
+    let location_header = document.getElementById("location_header");
     location_header.textContent = weatherData.location;
 
     let weather_overview = document.getElementById("weather_overview");
@@ -13,25 +13,29 @@ function updateDOM(weatherData) {
     current_temp.textContent =  weatherData.current_temp;
 
     let high_temp = document.getElementById("high_temp")
-    high_temp.textContent = "It could get up to " + weatherData.high_temp;
+    high_temp.textContent = weatherData.high_temp;
 
     let low_temp = document.getElementById("low_temp")
-    low_temp.textContent = "And as low as " + weatherData.low_temp;
+    low_temp.textContent = weatherData.low_temp;
 
     let humidity = document.getElementById("humidity");
-    humidity.textContent = "Humidity: " + weatherData.humidity;
+    humidity.textContent = weatherData.humidity;
 
     let wind_data = document.getElementById("wind_data")
-    wind_data.textContent = `Wind: ${weatherData.wind_dir} at ${weatherData.wind_speed} mph`
+    wind_data.textContent = `${weatherData.wind_dir} at ${weatherData.wind_speed}`
 }
 
 function toggleUnits(isMetric) {
-    let weather_data = document.querySelectorAll(".current_weather_container");
+    let weather_data = document.querySelectorAll(".weather_data");
+    let toggle_units_btn = document.getElementById("toggle_units_btn");
     weather_data.forEach(entry => {
         if (!isMetric) {
             entry.classList.add("metric");
+            toggle_units_btn.textContent = "Display Farhenheit";
+
         } else {
             entry.classList.remove("metric");
+            toggle_units_btn.textContent = "Display Celcius";
         }
     })
 }
